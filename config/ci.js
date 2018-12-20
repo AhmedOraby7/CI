7bf471407f282ec2f470d4eb926039f9982fa51b
Ahmed Oraby <es-Ahmed.Oraby@alexu.edu.eg>
2018-12-20 22:01:10 +0200

    first commit

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '504313431631-ruiqnq3rrt5pfomfsdd1h2cceob9rhh6.apps.googleusercontent.com',
+    googleClientSecret: 'jmf7FU7nqR7ciftUIga-J2ch',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: ' redis://127.0.0.1:6379'
+};
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '504313431631-ruiqnq3rrt5pfomfsdd1h2cceob9rhh6.apps.googleusercontent.com',
+  googleClientSecret: 'jmf7FU7nqR7ciftUIga-J2ch',
+  mongoURI: 'mongodb://orabi2:ahmed555A@ds119024.mlab.com:19024/blog',
+  cookieKey: '123123123',
+  redisUrl: ' redis://127.0.0.1:6379'
+};
+
